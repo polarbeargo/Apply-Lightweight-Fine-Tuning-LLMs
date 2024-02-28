9b8dea6875848718481daad3828d27bae998cfab
Hsin-Wen Chang <bow1226@gmail.com>
2024-02-27 19:37:38 -0500

    chore: Fix valueError: Tokenizer class GemmaTokenizer does not exist

--- installLightweightFineTuningLLMs.sh
+++ installLightweightFineTuningLLMs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pip3 install transformers
+pip3 -q install git+https://github.com/huggingface/transformers.git
 pip3 install torch torchvision
 pip3 install peft
 pip3 install numpy
@@ -8,3 +8,4 @@ pip3 install evaluate
 pip3 install sklearn
 pip3 install matplotlib
 pip3 install ipywidgets
+pip3 install bitsandbytes
