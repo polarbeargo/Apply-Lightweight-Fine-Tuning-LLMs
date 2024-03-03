c40c4c876a5cb1e38c6ab65db7c88be85b846a42
Hsin-Wen Chang <bow1226@gmail.com>
2024-03-03 18:18:40 -0500

    chore: Update installLightweightFineTuningLLMs.sh

--- installLightweightFineTuningLLMs.sh
+++ installLightweightFineTuningLLMs.sh
@@ -6,9 +6,9 @@ pip3 install wandb
 pip3 install torch torchvision
 pip3 install peft
 pip3 install numpy
-pip3 install datasets
 pip3 install evaluate
 pip3 install sklearn
 pip3 install matplotlib
 pip3 install ipywidgets
 pip3 install bitsandbytes
+pip3 install datasets==2.16.0
